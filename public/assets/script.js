b12f5b7d5bd02cc2450067ad0278d8544d24f12e
MahwishAli <mahwish11ali@gmail.com>
2023-11-23 17:02:05 +0500

    issues of links

---
+++ public/assets/script.js
@@ -0,0 +1,106 @@
+window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "UA-21798528-1");
+
+// second
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-GX6NLP153J");
+
+//   Hotjar Tracking Code for https://www.mydispute.ae 
+(function (h, o, t, j, a, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+    h._hjSettings = { hjid: 1040377, hjsv: 6 };
+    a = o.getElementsByTagName("head")[0];
+    r = o.createElement("script");
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+
+//   4th
+
+var wts = document.createElement("script");
+    wts.async = true;
+    wts.src = "https://app.ardalio.com/log7.js";
+    document.head.appendChild(wts);
+    wts.onload = function () {
+      wtslog7(2142757, 3);
+    };
+
+// 5th
+(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-PVS7NZL");
+
+// 6th
+adroll_adv_id = "2HOYAY24RNF2RNFLQ7LHMY";
+    adroll_pix_id = "UOIM2I4VFVAFTPKYUGHPJJ";
+    /* OPTIONAL: provide email to improve user identification */
+    /* adroll_email = "username@example.com"; */
+    (function () {
+      var _onload = function () {
+        if (
+          document.readyState &&
+          !/loaded|complete/.test(document.readyState)
+        ) {
+          setTimeout(_onload, 10);
+          return;
+        }
+        if (!window.__adroll_loaded) {
+          __adroll_loaded = true;
+          setTimeout(_onload, 50);
+          return;
+        }
+        var scr = document.createElement("script");
+        var host =
+          "https:" == document.location.protocol
+            ? "https://s.adroll.com"
+            : "http://a.adroll.com";
+        scr.setAttribute("async", "true");
+        scr.type = "text/javascript";
+        scr.src = host + "/j/roundtrip.js";
+        (
+          (document.getElementsByTagName("head") || [null])[0] ||
+          document.getElementsByTagName("script")[0].parentNode
+        ).appendChild(scr);
+      };
+      if (window.addEventListener) {
+        window.addEventListener("load", _onload, false);
+      } else {
+        window.attachEvent("onload", _onload);
+      }
+    })();
+
+    // 7th
+
+    if (self === top) {
+        var antiClickjack = document.getElementById("antiClickjack");
+        antiClickjack.parentNode.removeChild(antiClickjack);
+      } else {
+        top.location = self.location;
+      }
+
+    // for how it works image animation
+
+    
+
